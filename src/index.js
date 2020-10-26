b56b6c09a75260781ba6b85b6baa74dbc9b03e49
right42 <right42>
2020-10-26 18:17:26 +0900

    state & lifecycle

--- src/index.js
+++ src/index.js
@@ -1,28 +1,70 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-const user = {
-  firstName : 'Harper',
-  lastName : 'Perzk'
+
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      date : new Date()
+    };
+  }
+
+  componentDidMount(){
+    this.timerId = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+  
+  componentWillUnmount(){
+    clearInterval(this.timerId);
+  }
+
+  tick(){
+    this.setState({
+      date : new Date()
+    });
+  }
+
+  render(){
+    return (
+      <div>
+        <h1>Hello, World</h1>
+        <h2>It is {this.state.date.toLocaleTimeString()}</h2>
+      </div>
+    )
+  }
 }
 
-const element = (
-  <h1>
-    Hello, {formatName(user)}!
-  </h1>
-)
-
-function formatName(user){
-  return user.firstName + ' ' + user.lastName;
-}
-
-ReactDOM.render(
-    element,
+function tick(){
+  ReactDOM.render(
+    <Clock/>,
     document.querySelector('#root')
-)
+  )
+}
+
+setInterval(tick, 1000);
+
+
+// setInterval(tick, 1000);
+
+// function tick() {
+//   const element = (
+//     <div>
+//       <h1>Hello world.</h1>
+//       <h1>It is {new Date().toLocaleTimeString()}</h1>
+//     </div>
+//   );
+
+//   ReactDOM.render(
+//     element,
+//     document.querySelector('#root')
+//   );
+// }
+
 
 // ReactDOM.render(
 //   <React.StrictMode>
@@ -31,7 +73,28 @@ ReactDOM.render(
 //   document.getElementById('root')
 // );
 
+// function Welcome(props) {
+//   return <h1>Hello, {props.name}</h1>;
+// }
+
+// class WelcomeClass extends React.Component {
+//   render(){
+//     return <h1>Hello, {this.props.name}</h1>;
+//   }
+// }
+
+// function App(){
+//   return (
+//     <div>
+//       <WelcomeClass name="Sara"/>
+//       <WelcomeClass name="Cahal"/>
+//       <Welcome name="Edite"/>
+//     </div>
+//   )
+// }
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
