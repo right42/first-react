d52da858b82c2c04d0f586a4532c16b6fe09dbed
right42 <chjm452@naver.com>
2020-10-26 23:35:55 +0900

    conditional rendering

--- src/index.js
+++ src/index.js
@@ -3,95 +3,127 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 
+function WarningBanner(props){
+  if(!props.warn){
+    return null;
+  }
 
-class Clock extends React.Component {
-  constructor(props) {
-    super(props);
+  return (
+    <div className="warning">
+      Warning!
+    </div>
+  )
+}
+
+
+class Page extends React.Component {
+  constructor (props) {
+    super(props)
     this.state = {
-      date : new Date()
+      showWaring : true
     };
+    this.handleToggleClick = this.handleToggleClick.bind(this);
   }
 
-  componentDidMount(){
-    this.timerId = setInterval(
-      () => this.tick(),
-      1000
-    );
-  }
-  
-  componentWillUnmount(){
-    clearInterval(this.timerId);
-  }
-
-  tick(){
-    this.setState({
-      date : new Date()
-    });
+  handleToggleClick(){
+    this.setState((state) => ({
+      showWaring : !state.showWaring
+    }));
   }
 
   render(){
     return (
       <div>
-        <h1>Hello, World</h1>
-        <h2>It is {this.state.date.toLocaleTimeString()}</h2>
+        <WarningBanner warn={this.state.showWaring}/>
+        <button onClick={this.handleToggleClick}>
+          {this.state.showWaring ? 'hide' : 'show'}
+        </button>
       </div>
     )
   }
 }
 
-function tick(){
-  ReactDOM.render(
-    <Clock/>,
-    document.querySelector('#root')
-  )
-}
-
-setInterval(tick, 1000);
+ReactDOM.render(
+  <Page />,
+  document.querySelector('#root')
+)
 
 
-// setInterval(tick, 1000);
+// class LoginControl extends React.Component {
+//   constructor (props) {
+//     super(props);
+//     this.handleLoginClick = this.handleLoginClick.bind(this);
+//     this.handleLogoutClick = this.handleLogoutClick.bind(this);
+//     this.state = {
+//       isLoggedIn : false
+//     }
+//   }
 
-// function tick() {
-//   const element = (
-//     <div>
-//       <h1>Hello world.</h1>
-//       <h1>It is {new Date().toLocaleTimeString()}</h1>
-//     </div>
-//   );
+//   handleLoginClick(){
+//     this.setState({isLoggedIn : true});
+//   }
 
-//   ReactDOM.render(
-//     element,
-//     document.querySelector('#root')
-//   );
-// }
+//   handleLogoutClick(){
+//     this.setState({isLoggedIn : false});
+//   }
 
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
-
-// function Welcome(props) {
-//   return <h1>Hello, {props.name}</h1>;
-// }
-
-// class WelcomeClass extends React.Component {
 //   render(){
-//     return <h1>Hello, {this.props.name}</h1>;
+//     const isLoggedIn = this.state.isLoggedIn;
+//     let button;
+//     if(isLoggedIn){
+//       button = <LogoutButton onClick={this.handleLogoutClick}/>
+//     } else {
+//       button = <LoginButton onClick={this.handleLoginClick}/>
+//     }
+
+//     return (
+//       <div>
+//         <Greeting isLoggedIn={isLoggedIn} />
+//         {button}
+//       </div>
+//     )
 //   }
 // }
 
-// function App(){
+// function LoginButton(props){
 //   return (
-//     <div>
-//       <WelcomeClass name="Sara"/>
-//       <WelcomeClass name="Cahal"/>
-//       <Welcome name="Edite"/>
-//     </div>
+//     <button onClick={props.onClick}>
+//       Login
+//     </button>
 //   )
 // }
+
+// function LogoutButton(props){
+//   return (
+//     <button onClick={props.onClick}>
+//       Logout
+//     </button>
+//   )
+// }
+
+
+// function UseGreeting(){
+//   return <h1>Welcome back!</h1>
+// }
+
+// function GuestGreeting(){
+//   return <h1>Please sign up.</h1>
+// }
+
+// function Greeting(props){
+//   const isLoggedIn = props.isLoggedIn;
+//   if(isLoggedIn){
+//     return <UseGreeting/>
+//   } else {
+//     return <GuestGreeting/>
+//   }
+// }
+
+// ReactDOM.render(
+//   <LoginControl />,
+//   document.querySelector('#root')
+// )
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
